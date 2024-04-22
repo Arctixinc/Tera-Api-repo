adc7d9b50cdcd466744e01ea23151d7c2ffa2a20
Arctixinc <105559773+Arctixinc@users.noreply.github.com>
2024-04-23 01:56:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import asyncio
 from flask import Flask, request, jsonify
 from tera import terabox
-from server import teraBoxDl
+from server import teraBoxD
+from tera2 import get_data
 
 app = Flask(__name__)
 
@@ -35,6 +36,18 @@ async def get_download_link():
 
     download_link = await teraBoxDl(data)
     return {"download_link": download_link}
+    
+@app.route('/api2', methods=['GET'])
+async def get_download_data():
+    url = request.args.get('url')
+    if not url:
+        return jsonify({"error": "URL parameter is missing"}), 400
+
+    data = await get_data(url)
+    if data:
+        return jsonify(data)
+    else:
+        return jsonify({"error": "Failed to fetch data"}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
